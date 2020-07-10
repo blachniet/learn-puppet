0fe796df26c7d862be3299f79fe7c9f8768f4c30
Brian Lachniet <blachniet@gmail.com>
2020-07-09 20:41:27 -0400

    Add my first variable array

--- puppetmaster/prod-manifests/nodes.pp
+++ puppetmaster/prod-manifests/nodes.pp
@@ -3,6 +3,13 @@ node 'puppetnode.localdomain' {
 }
 
 class linux {
+
+  $admintools = ['git', 'vim', 'screen']
+
+  package { $admintools:
+    ensure => installed,
+  }
+
   file { '/info.txt':
     ensure => present,
     content => inline_template("Created by Puppet at <%= Time.now %>\n"),
