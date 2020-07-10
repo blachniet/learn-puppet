6d58eddc2e67612f9f5451dc75c55a82e19d933f
Brian Lachniet <blachniet@gmail.com>
2020-07-09 20:35:31 -0400

    Create my first class

--- puppetmaster/prod-manifests/nodes.pp
+++ puppetmaster/prod-manifests/nodes.pp
@@ -1,4 +1,8 @@
 node 'puppetnode.localdomain' {
+  class { 'linux': }
+}
+
+class linux {
   file { '/info.txt':
     ensure => present,
     content => inline_template("Created by Puppet at <%= Time.now %>\n"),
